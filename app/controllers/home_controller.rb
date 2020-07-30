f486dcc233db212be9974bac27096159907f1a9c
Peter Andersson <pjotor87@hotmail.com>
2020-07-30 14:38:41 +0200

    Imported old rspec files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,18 @@
+require 'sinatra'
+require 'net/http'
+require "uri"
+
 class HomeController < ApplicationController
+  get '/' do
+    "Returnable string..."
+  end
+
+  get '/req/' do
+    uri = URI.parse("http://thaiembassy.se/en/home/")
+    http = Net::HTTP.new(uri.host, uri.port)
+    response = http.request(Net::HTTP::Get.new(uri.request_uri))
+    body response.body
+  end
 end
+
+#run HomeController.run!
